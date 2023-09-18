2e5a1212ceb1d19478540fb3d7653d91c1435c9e
Marek Lukáš <tajnymag@gmail.com>
2023-09-18 11:45:21 +0200

    WIP: minor playground changes

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -6,39 +6,43 @@
 #include <memory>
 #include <esp_mqtt.hpp>
 #include <esp_mqtt_client_config.hpp>
+#include <list>
+#include <functional>
 
 #include "services/inkplate_static.h"
-#include "mqtt/topics/draw_bitmap_1bit/handler.hpp"
 
 namespace mqtt = idf::mqtt;
 
 static const char *TAG = "Main";
 
+struct VSBEinkMQTTMHandler {
+    mqtt::Filter filter;
+    std::function<void(const esp_mqtt_event_handle_t)> callback;
+    mqtt::QoS qos;
+};
+
 class MyClient final : public mqtt::Client {
 public:
     using mqtt::Client::Client;
 
-    MyClient(const std::string& base_topic, const mqtt::BrokerConfiguration& broker, const mqtt::ClientCredentials& credentials, const mqtt::Configuration& config)
-            : mqtt::Client{broker, credentials, config}, messages(base_topic + "messages/received"), sent_load(base_topic + "load/+/sent"){}
+    MyClient(const mqtt::BrokerConfiguration& broker, const mqtt::ClientCredentials& credentials, const mqtt::Configuration& config)
+            : mqtt::Client{broker, credentials, config} {}
 
 private:
-    void on_connected(const esp_mqtt_event_handle_t event) override
-    {
-        using mqtt::QoS;
-        subscribe(messages.get());
-        subscribe(sent_load.get(), QoS::AtMostOnce);
-    }
-    void on_data(const esp_mqtt_event_handle_t event) override
-    {
-        printf("Subscribed to %.*s\r\n", event->topic_len, event->topic);
-        if (messages.match(event->topic, event->topic_len)) {
-            ESP_LOGI(TAG, "Received in the %.*s topic", event->topic_len, event->topic);
-        } else if (sent_load.match(event->topic, event->topic_len)) {
-            ESP_LOGI(TAG, "Received in the sent_load topic");
+    std::list<VSBEinkMQTTMHandler> handlers;
+
+    void on_connected(const esp_mqtt_event_handle_t event) override{
+        for (auto& handler : handlers) {
+            subscribe(handler.filter.get(), handler.qos);
         }
     }
-    mqtt::Filter messages{""};
-    mqtt::Filter sent_load{""};
+    void on_data(const esp_mqtt_event_handle_t event) override {
+        for (auto& handler : handlers) {
+            if (handler.filter.match(event->topic, event->topic_len)) {
+                handler.callback(event);
+            }
+        }
+    }
 };
 
 [[noreturn]]
