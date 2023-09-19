0a9dff4095923487a731f8b1d4ccdb9d690a3170
Marek Lukáš <tajnymag@gmail.com>
2023-09-19 11:31:21 +0200

    WIP: playing with cJSON

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,4 +1,3 @@
-#include <esp_crt_bundle.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_netif.h>
@@ -8,6 +7,7 @@
 #include <esp_mqtt_client_config.hpp>
 #include <list>
 #include <functional>
+#include <cJSON.h>
 
 #include "services/inkplate_static.h"
 
@@ -15,21 +15,22 @@ namespace mqtt = idf::mqtt;
 
 static const char *TAG = "Main";
 
-struct VSBEinkMQTTMHandler {
+struct MQTTMessageHandler {
     mqtt::Filter filter;
     std::function<void(const esp_mqtt_event_handle_t)> callback;
     mqtt::QoS qos;
 };
 
-class MyClient final : public mqtt::Client {
+class MQTTClient final : public mqtt::Client {
 public:
     using mqtt::Client::Client;
 
-    MyClient(const mqtt::BrokerConfiguration& broker, const mqtt::ClientCredentials& credentials, const mqtt::Configuration& config)
-            : mqtt::Client{broker, credentials, config} {}
+    MQTTClient(const mqtt::BrokerConfiguration& broker, const mqtt::ClientCredentials& credentials, const mqtt::Configuration& config) = delete;
+    MQTTClient(const mqtt::BrokerConfiguration& broker, const mqtt::ClientCredentials& credentials, const mqtt::Configuration& config, const std::list<MQTTMessageHandler>& handlers)
+            : mqtt::Client{broker, credentials, config}, handlers{handlers} {}
 
 private:
-    std::list<VSBEinkMQTTMHandler> handlers;
+    std::list<MQTTMessageHandler> handlers;
 
     void on_connected(const esp_mqtt_event_handle_t event) override{
         for (auto& handler : handlers) {
@@ -63,7 +64,35 @@ void mainTask(void *param) {
     mqtt::ClientCredentials credentials{};
     mqtt::Configuration config{};
 
-    MyClient client{"$SYS/broker/", broker, credentials, config};
+    MQTTClient client{
+            broker,
+            credentials,
+            config,
+            {
+                        {
+                            mqtt::Filter{std::string{"inkplate/+/display"}},
+                            [](const esp_mqtt_event_handle_t event) {
+                                ESP_LOGI(TAG, "Received message on topic %.*s", event->topic_len, event->topic);
+                                cJSON* message = cJSON_Parse(event->data);
+                                if (message == nullptr) {
+                                    ESP_LOGE(TAG, "Failed to parse message");
+                                    cJSON_Delete(message);
+                                    return;
+                                }
+                                cJSON* message_text = cJSON_GetObjectItem(message, "text");
+                                if (message_text == nullptr) {
+                                    ESP_LOGE(TAG, "Failed to parse message text");
+                                    cJSON_Delete(message);
+                                    return;
+                                }
+                                ESP_LOGI(TAG, "Message text: %s", message_text->valuestring);
+                                ESP_LOGI(TAG, "[APP] Free memory: %lu bytes", esp_get_free_heap_size());
+                                cJSON_Delete(message);
+                            },
+                            mqtt::QoS::AtLeastOnce
+                        }
+            }
+    };
 
     while (true) {
         constexpr TickType_t xDelay = 500 / portTICK_PERIOD_MS;
